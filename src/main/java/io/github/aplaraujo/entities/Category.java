e44fc4b08fb98ad527506179f4b3c06f6c3c8260
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-28 20:20:52 -0300

    Audit data

--- src/main/java/io/github/aplaraujo/entities/Category.java
+++ src/main/java/io/github/aplaraujo/entities/Category.java
@@ -3,6 +3,8 @@ package io.github.aplaraujo.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
+
 @Entity
 @Table(name = "tb_category")
 @Getter
@@ -17,4 +19,20 @@ public class Category {
 
     @Column(name = "name")
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 }
