386b1a99b3f5bbd1c0df1ed6af00606167196b87
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-11-27 21:26:56 -0300

    Exception treatment

---
+++ src/main/java/io/github/aplaraujo/dto/CustomError.java
@@ -0,0 +1,11 @@
+package io.github.aplaraujo.dto;
+
+import java.time.Instant;
+
+public record CustomError(
+        Instant timestamp,
+        Integer status,
+        String error,
+        String path
+) {
+}
