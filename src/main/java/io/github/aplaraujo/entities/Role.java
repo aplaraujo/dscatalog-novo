678e7734b2180d41ae842f864927cf3898ae831b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-23 20:12:37 -0300

    Role entity and enum

---
+++ src/main/java/io/github/aplaraujo/entities/Role.java
@@ -0,0 +1,22 @@
+package io.github.aplaraujo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_role")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private String authority;
+}
---
+++ src/main/java/io/github/aplaraujo/entities/enums/RoleEnum.java
@@ -0,0 +1,5 @@
+package io.github.aplaraujo.entities.enums;
+
+public enum RoleEnum {
+    ROLE_ADMIN, ROLE_USER
+}
